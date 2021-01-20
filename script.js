89b98e7daeca0db7341f97465efb5d52092b0113
EderIRE <71097226+EderIRE@users.noreply.github.com>
2021-01-20 20:32:39 +0000

    button for DarkMode

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const btn = document.getElementById('click');
-const pic = document.getElementsByClassName('dark');
+const body = document.getElementsByTagName('BODY')[0];
+const btn = document.getElementById('switchBtn');
+const pic = document.querySelectorAll('IMG');
 
 btn.addEventListener('click', function () {
-  document.body.style.filter = 'invert(1) hue-rotate(180deg)';
+  body.classList.toggle('darkMode');
+  for (item of pic) {
+    item.classList.toggle('darkMode');
+  }
 });
