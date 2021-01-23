ea606fd88fb0d0ef746d1da33b412d4227f40a2b
EderIRE <71097226+EderIRE@users.noreply.github.com>
2021-01-23 14:38:44 +0000

    remove some code

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const body = document.getElementsByTagName('BODY')[0];
 const btn = document.getElementById('switchBtn');
 const pic = document.querySelectorAll('IMG');
 
 btn.addEventListener('click', function () {
-  body.classList.toggle('darkMode');
+  document.body.classList.toggle('darkMode');
   for (item of pic) {
     item.classList.toggle('darkMode');
   }
